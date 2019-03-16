8c3330bfd6919120f9d4afb18ade2de8274ddfb5
Srinivasansriram <srinivasansriram1998@gmail.com>
2019-03-16 19:24:18 +0530

    updated 0.0

---
+++ chit-chatbackend/src/main/java/com/srini/chit_chatbackend/App.java
@@ -0,0 +1,13 @@
+package com.srini.chit_chatbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
